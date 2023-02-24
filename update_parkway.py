8658b597c3ff92edf458992c31faeae16779c212
Jeff Bliss <jeff.a.bliss@gmail.com>
2023-02-24 17:02:49 -0500

    it should now be working. output.kml is generated and looks to be good

--- update_parkway.py
+++ update_parkway.py
@@ -2,9 +2,20 @@ import requests, pprint
 from bs4 import BeautifulSoup
 from fastkml import kml
 
+pp = pprint.PrettyPrinter()
+
+#### CONSTANTS
+style_constants_dict = {
+    'line_closed': '#line-A52714-4100',
+    'line_open': '#line-558B2F-4100',
+    'line_ungated': '#line-7CB342-4294',
+    'icon_open': '#icon-1739-558B2F',
+    'icon_closed': '#icon-1739-A52714'
+}
+
+#### SCRAPING PARKWAY OPEN/CLOSED STATUS
 table_data = []
-virginia_parkway_status = []
-nc_parkway_status = []
+parkway_status = []
 url = 'https://www.nps.gov/blri/planyourvisit/roadclosures.htm'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'html.parser')
@@ -14,7 +25,7 @@ tables = soup.findAll('table')
 virginia_parkway_table = tables[1]
 nc_parkway_table = tables[2]
 
-# find all the rows in the table and throw it into table_data list
+# find all the rows in the virginia table and throw it into table_data list
 table_body = virginia_parkway_table.find('tbody')
 rows = table_body.find_all('tr')
 for row in rows:
@@ -22,15 +33,7 @@ for row in rows:
     cols = [ele.text.strip() for ele in cols]
     table_data.append([ele for ele in cols if ele])
 
-# create dictionary from table_data and push into parkway_status list
-for row in table_data:
-    if len(row) == 4:
-        dict = {'mileposts': row[0], 'crossroads': row[1], 'status': row[2], 'notes': row[3]}
-    else:
-        dict = {'mileposts': row[0], 'crossroads': row[1], 'status': row[2]}
-    virginia_parkway_status.append(dict)
-
-# find all the rows in the table and throw it into table_data list
+# find all the rows in the nc table and throw it into table_data list
 table_body = nc_parkway_table.find('tbody')
 rows = table_body.find_all('tr')
 for row in rows:
@@ -40,31 +43,81 @@ for row in rows:
 
 # create dictionary from table_data and push into parkway_status list
 for row in table_data:
+    mileposts = row[0]
+    # remove all newlines and & from crossroads
+    crossroads = row[1].replace("\n", " ")
+    crossroads = crossroads.replace("&", "and")
+    # Remove all asterisks from Ungated*
+    status = row[2].replace("*", "")
     if len(row) == 4:
-        dict = {'mileposts': row[0], 'crossroads': row[1], 'status': row[2], 'notes': row[3]}
+        notes = row[3]
+        dict = {'mileposts': mileposts, 'crossroads': crossroads, 'status': status, 'notes': notes}
     else:
-        dict = {'mileposts': row[0], 'crossroads': row[1], 'status': row[2]}
-    nc_parkway_status.append(dict)
+        dict = {'mileposts': mileposts, 'crossroads': crossroads, 'status': status}
+    parkway_status.append(dict)
 
-pp = pprint.PrettyPrinter()
-pp.pprint(virginia_parkway_status)
-pp.pprint(nc_parkway_status)
+# pp.pprint(parkway_status)
+
+#### READING KML FILE AND MODIFYING IT WITH NEW STATUS
+kml_file = 'modified_name.kml'
+with open(kml_file, 'rt') as myfile:
+    doc=myfile.read().encode('utf-8')
+
+k=kml.KML()
+k.from_string(doc)
+
+features = list(k.features())
+features_list = list(features[0].features())
+
+""" Two foor loops to go through the parkway_status dict and the kml file Placemarks
+    compare names and then break out of inner for loop if there is a match.
+    There are currently a handful of non matches that need to be added to the kml file.
+    For every match, update the description, styleUrl, and extended data status and notes"""
+for placemark in features_list:
+    match = False
+    mileposts = None
+    crossroads = None
+    status = None
+    notes = None
+    styleUrl = 'line'
+
+    if (placemark.styleUrl.split('-')[0] == '#icon'): # check if style is line or icon
+        styleUrl = 'icon'
     
+    for row in parkway_status:
+        if match == True:
+            break
+        mileposts = row['mileposts']
+        crossroads = row['crossroads']
+        status = row['status']
+        notes = 'No Notes'
+        if len(row) == 4:
+            notes = row['notes']
+        if placemark.name == crossroads:
+            match = True
+   
+    if match == True:
+        placemark.description = f'Mileposts: {mileposts}<br>Status: {status}<br>Notes: {notes}'
 
-#Read file into string and convert to UTF-8 (Python3 style)
-#with open('doc.kml', 'rt') as myfile:
-#    doc = myfile.read().encode('utf-8')
+        # probably a better way to do this style line silliness
+        if styleUrl == 'line' and status == 'Open':
+            placemark.styleUrl = style_constants_dict['line_open']
+        elif styleUrl == 'line' and status == 'Closed':
+            placemark.styleUrl = style_constants_dict['line_closed']
+        elif styleUrl == 'line' and (status == 'Ungated' or status == 'Ungated*'):
+            placemark.styleUrl = style_constants_dict['line_ungated']
+        elif styleUrl == 'icon' and status == 'Open':
+            placemark.styleUrl = style_constants_dict['icon_open']
+        else: # assume icon style of closed
+            placemark.styleUrl = style_constants_dict['icon_closed']
 
-# Create the KML object to store the parsed result
-#k = kml.KML()
+        for element in placemark.extended_data.elements:
+            if element.name == 'status':
+                element.value = status
+            elif element.name == 'notes':
+                element.value = notes
+       
+#for f in f2[0].extended_data.elements:
 
-# Read in the KML string
-#k.from_string(doc)
-
-# Get features
-#features = list(k.features())
-#placemarks_list = list(features[0].features())
-
-#for placemark in placemarks_list:
-#    print(placemark.extended_data.elements)
-
+with open("output.kml", "wt") as f:
+    f.writelines(k.to_string(prettyprint=True))
